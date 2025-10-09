b9c01d1770529c7ecfd4d1cea93d38d6af9994ed
Khanbite <167370853+Khanbite@users.noreply.github.com>
2025-10-08 22:17:55 -0400

    Update MaxDifference.java

--- MaxDifference.java
+++ MaxDifference.java
@@ -3,19 +3,25 @@ Name: Khani Lyan
 Date: 10/2/2025
 Description: This program finds the max difference in a user-defined array of variable length.
 */
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class MaxDifference {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter numbers: ");
+        System.out.println("Enter numbers separated by spaces: ");
 
-        for (int i = 0; i < 15; i++) {
-            userNums[i] = input.nextInt();
+        String line = input.nextLine();
+        String[] individual = line.split(" ");
+
+        int[] userNums = new int[individual.length];
+        for (int i = 0; i < individual.length; i++) {
+            userNums[i] = Integer.parseInt(individual[i]);
         }
 
+        int difference = maxDifference(userNums);
+        System.out.println("Max difference: " + difference);
     }
+
     public static int maxDifference(int... userList) {
         int max = userList[0];
         int min = userList[0];
@@ -23,8 +29,7 @@ public class MaxDifference {
         for (int num : userList) {
             if (num > max) max = num;
             if (num < min) min = num;
-        } System.out.println(max);
-        System.out.println(min);
+        }
         return max - min;
     }
 }
